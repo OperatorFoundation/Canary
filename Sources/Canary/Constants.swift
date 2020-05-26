956920a2e2f2e13bf5b43ce2f6455e736a388585
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2020-05-25 22:20:59 -0500

    Removing web tests

--- Sources/Canary/Constants.swift
+++ Sources/Canary/Constants.swift
@@ -40,16 +40,18 @@ let replicant = Transport(name: "Replicant", port: replicantServerPort)
 let meek = Transport(name: "meeklite", port: meekServerPort)
 
 let webTest = Transport(name: "webTest", port: "443")
-let allTransports = [replicant, obfs4iatMode, shadowsocks, obfs4, obfs2, meek]
-let testWebAddresses = ["https://www.reddit.com/",
-                        "https://twitter.com/home",
-                        "https://www.facebook.com/",
-                        "https://www.youtube.com/",
-                        "https://www.netflix.com/",
-                        "https://www.instagram.com/",
-                        "https://www.hulu.com/",
-                        "https://www.cnn.com/",
-                        "https://www.wikipedia.org/"]
+let allTransports = [replicant, obfs4iatMode, shadowsocks, obfs4, obfs2]
+//meek
+//let testWebAddresses = ["https://www.facebook.com/",
+//                        "https://www.youtube.com/",
+//                        "https://www.netflix.com/",
+//                        "https://www.instagram.com/",
+//                        "https://www.hulu.com/",
+//                        "https://www.cnn.com/",
+//                        "https://www.wikipedia.org/"]
+//
+////"https://www.reddit.com/",
+////"https://twitter.com/home",
 
 let stateDirectoryPath = "TransportState"
 
