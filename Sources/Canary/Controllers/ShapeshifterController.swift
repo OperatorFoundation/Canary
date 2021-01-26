af9343405695ea8e3bfbbf904d95bb2a5716f726
consuelita <adelita@operatorfoundation.org>
2021-01-25 19:07:08 -0600

    Update ShapeshifterController.swift

--- Sources/Canary/Controllers/ShapeshifterController.swift
+++ Sources/Canary/Controllers/ShapeshifterController.swift
@@ -83,6 +83,7 @@ class ShapeshifterController
                 print("\n🛑 shapeshifter-dispatcher failed to launch.")
             }
 
+            print("🧩 Launched shapeshifter-dispatcher. 🧩")
             return launchTask!.isRunning
         }
         else
